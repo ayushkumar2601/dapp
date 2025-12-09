7dab264f29745b2f7c9dd5e175eb20a8a5bc8a17
Ayush <kumarayush.professional@gmail.com>
2025-12-09 12:01:44 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+// import './App.css'
+function App() {
+  // const [count, setCount] = useState(0)
+  return (
+    <div>
+      hi there I am <b>Ayush Kumar.</b>
+    </div>
+  )
+}
+
+export default App
